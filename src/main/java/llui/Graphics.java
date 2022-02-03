707b8437c58b04c9b368b33103d560a5397855be
scintilla4evr <kacper17@outlook.com>
2022-02-03 03:56:39 +0100

    Initial commit - code from ui-server-api

---
+++ src/main/java/llui/Graphics.java
@@ -0,0 +1,113 @@
+package llui;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class Graphics {
+
+    public Graphics() {
+    }
+
+    public void drawLine(RequestGenerator req, int sx, int sy, int ex, int ey, int r, int g, int b) {
+        byte[] arr = {
+                (byte) 16,
+                (byte) sx,
+                (byte) sy,
+                (byte) ex,
+                (byte) ey,
+                (byte) r, (byte) g, (byte) b
+        };
+        req.addByteArrayToRequest(arr);
+    }
+
+    public void fillEllipse(RequestGenerator req, int x, int y, int width, int height, int r, int g, int b) {
+        byte[] arr = {
+                (byte) 17,
+                (byte) x,
+                (byte) y,
+                (byte) width,
+                (byte) height,
+                (byte) r, (byte) g, (byte) b
+        };
+        req.addByteArrayToRequest(arr);
+    }
+
+    public void fillRect(RequestGenerator req, int x, int y, int width, int height, int r, int g, int b) {
+        byte[] arr = {
+                (byte) 18,
+                (byte) x,
+                (byte) y,
+                (byte) width,
+                (byte) height,
+                (byte) r, (byte) g, (byte) b
+        };
+        req.addByteArrayToRequest(arr);
+    }
+
+    public void drawQuadratic(RequestGenerator req, int sx, int sy, int cx, int cy, int ex, int ey, int r, int g, int b) {
+        byte[] arr = {
+                (byte) 18,
+                (byte) sx,
+                (byte) sy,
+                (byte) cx,
+                (byte) cy,
+                (byte) ex,
+                (byte) ey,
+                (byte) r, (byte) g, (byte) b
+        };
+        req.addByteArrayToRequest(arr);
+    }
+
+    public void drawPolygon(RequestGenerator req, int[] xs, int[] ys, int r, int g, int b) {
+        byte[] array = new byte[2 * xs.length + 5];
+        array[0] = (byte) 23;
+        array[1] = (byte) xs.length;
+
+        for (int i = 2; i < 2 * xs.length + 2; i++) {
+            array[i] = i % 2 == 0 ? (byte) xs[(i - 2) / 2] : (byte) ys[(i - 2) / 2];
+        }
+        array[array.length - 3] = (byte) r;
+        array[array.length - 2] = (byte) g;
+        array[array.length - 1] = (byte) b;
+
+        req.addByteArrayToRequest(array);
+    }
+
+    public void drawText(RequestGenerator req, int x, int y, String text, int r, int g, int b) {
+        byte[] arr1 = {
+                (byte) 30,
+                (byte) x,
+                (byte) y,
+                (byte) text.length()
+        };
+        byte[] arr2 = {
+                (byte) r,
+                (byte) g,
+                (byte) b
+        };
+        byte[] array = concatenateTwoArrays(
+                concatenateTwoArrays(
+                        arr1, text.getBytes(StandardCharsets.UTF_8)),
+                arr2);
+        req.addByteArrayToRequest(array);
+    }
+
+    public void drawIcon(RequestGenerator req, int x, int y, int iconNo, int r, int g, int b) {
+        byte[] arr = {
+                (byte) 22,
+                (byte) x,
+                (byte) y,
+                (byte) iconNo,
+                (byte) r, (byte) g, (byte) b
+        };
+        req.addByteArrayToRequest(arr);
+    }
+
+    public byte[] concatenateTwoArrays(byte[] array1, byte[] array2) {
+        byte[] result = Arrays.copyOf(array1, array1.length + array2.length);
+        System.arraycopy(array2, 0, result, array1.length, array2.length);
+        array2 = Arrays.copyOf(result, result.length);
+
+        return array2;
+    }
+}
---
+++ src/main/java/llui/RequestGenerator.java
@@ -0,0 +1,53 @@
+package llui;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Arrays;
+
+public class RequestGenerator {
+
+    private byte[] request;
+    private Socket serverSocket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public RequestGenerator() {
+        request = new byte[1];
+    }
+
+    public void beginRequest() {
+        request[0] = (byte) 0;
+    }
+
+    public void openSendClose(String hostname, int portNo) {
+        try {
+            serverSocket = new Socket(hostname, portNo);
+            out = new DataOutputStream(serverSocket.getOutputStream());
+            in = new DataInputStream((serverSocket.getInputStream()));
+            out.write(request);
+            serverSocket.close();
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void endRequest() {
+        request[request.length - 1] = (byte) 2;
+    }
+
+    public void addByteArrayToRequest(byte[] array) {
+        request[0] += 1;
+        byte[] result = Arrays.copyOf(request, request.length + array.length);
+        System.arraycopy(array, 0, result, request.length, array.length);
+        request = Arrays.copyOf(result, result.length + 1);
+        request[request.length - 1] = (byte) 1;
+    }
+
+    public byte[] getRequest() {
+        return request;
+    }
+
+}
